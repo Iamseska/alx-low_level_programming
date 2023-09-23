551d4f97957b7cb629bdf9bed14a85f2e86a5dc9
Okello <okelloalloys34@gmail.com>
2023-09-23 07:14:48 -0700

    the two

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,15 @@ int main(void)
 		n = rand() - RAND_MAX / 2
 		if (n > 0)
 		{
-			printf("%d is zero\n", n);
+			printf("%d is positive\n", n);
 		}
-	return (0);
+		else if (n == 0)
+		{
+			pritnf("%d is zero\n", n);
+		}
+		else
+		{
+			pritnf("%d is negative\n", n);
+		}
+		return (0);
 }
