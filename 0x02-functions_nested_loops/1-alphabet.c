c464e3763c199c98cefcd7af2355710abf65f400
Okello <okelloalloys34@gmail.com>
2023-11-01 08:15:51 -0700

    the goal

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * alphabet - contains the alphabet letters
+ *
+ * Return : void
+ */
+void alphabet(void)
+{
+	char lt;
+	for(lt = 'a'; lt <= 'z'; lt++)
+	putchar(lt);
+	putchar('\n');
+}
